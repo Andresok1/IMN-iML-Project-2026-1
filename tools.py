ca2486b7f19161833bac00e8f6f62e30f8041ca3
Andrés Ospina <andres.felipe.ospina.cardenas@stud.uni-hannover.de>
2026-01-24 20:59:05 +0100

    global_json_calculation, update_summary and pca_visualizartion for dim=2,3 added

--- tools.py
+++ tools.py
@@ -3,6 +3,8 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import os
+import json
 
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.metrics import silhouette_score
@@ -13,6 +15,8 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 
 def gower_hierarchical_clustering(X, y, categorical_cols, numerical_cols, plot_dendrogram=False, initial_cluster_len=None, clusters=None):
@@ -58,14 +62,14 @@ def gower_hierarchical_clustering(X, y, categorical_cols, numerical_cols, plot_d
             print(f"Cluster {i} size: {len(X_cluster)}")
 
 
-            # min_cluster_size = len(X_cluster)/total_cluster_size
-            min_cluster_size = 1300
+            min_cluster_size = len(X_cluster)/initial_cluster_len
 
 
-            if len(X_cluster) < min_cluster_size :
+            if min_cluster_size < 0.2 :          #No cluster should be smaller than 20% of the initial dataset size
                 check_size = False
-                print("small cluster was found in:", {len(X_cluster)})
-
+                print("Small cluster was found in:", {len(X_cluster)})
+                print("Stopping further division.")
+                break
 
         print("----------------------------")
 
@@ -77,7 +81,7 @@ def gower_hierarchical_clustering(X, y, categorical_cols, numerical_cols, plot_d
 
                 # If the cluster is large enough, recursively split it
 
-                print("i am going to divide cluster:", i, "with size:", len(X_cluster))
+                print("Dividing cluster:", i, "with size:", len(X_cluster))
                 sub_clusters, _ = gower_hierarchical_clustering(X_cluster, y_cluster, categorical_cols, numerical_cols, plot_dendrogram=False, initial_cluster_len=initial_cluster_len, clusters=clusters)
 
         else:
@@ -91,7 +95,7 @@ def gower_hierarchical_clustering(X, y, categorical_cols, numerical_cols, plot_d
             print("len1:", len1, "len2:", len2)
             #check which is the big cluster
 
-            if len1/initial_cluster_len > 0.5 or len2/initial_cluster_len > 0.5:
+            if len1/initial_cluster_len > 0.5 or len2/initial_cluster_len > 0.5:        #No cluster should be bigger than 50% of the initial dataset size
                 print("one dataframes assigned to clusters")
 
                 if len1/initial_cluster_len > 0.5:
@@ -174,8 +178,8 @@ def silhouette_score_criterion(gower_matrix,show_results=False):
     return -1, -1
 
 
-def pca_mixed_data_visualization(clusters, categorical_cols, numerical_cols,visualization=False):
-    
+
+def pca_mixed_data_visualization(clusters, categorical_cols, numerical_cols, visualization=False, dim=3):
 
     preprocess = ColumnTransformer(
         [
@@ -184,28 +188,87 @@ def pca_mixed_data_visualization(clusters, categorical_cols, numerical_cols,visu
         ]
     )
 
-    n_clusters = len(clusters)
+    if not visualization:
+        return
 
-    if visualization:
-        n_cols = 2
-        n_rows = int(np.ceil(n_clusters / n_cols))
+    #Whole graphic (All clusters)
+
+    X_all = []
+    cluster_ids = []
+
+    for i, (X_cluster, _) in clusters.items():
+        X_all.append(X_cluster)
+        cluster_ids.extend([i] * len(X_cluster))
+
+    X_all = pd.concat(X_all, axis=0)
+    cluster_ids = np.array(cluster_ids)
+
+    X_all_proc = preprocess.fit_transform(X_all)
+
+    pca_global = PCA(n_components=dim)
+    X_all_pca = pca_global.fit_transform(X_all_proc)
+
+    if dim==2:
+        plt.figure(figsize=(8, 6))
+        for i in clusters.keys():
+            mask = cluster_ids == i
+            plt.scatter(
+                X_all_pca[mask, 0],
+                X_all_pca[mask, 1],
+                alpha=0.7,
+                label=f"Cluster {i}"
+            )
+        plt.xlabel("PC1")
+        plt.ylabel("PC2")
+        
+        plt.title("PCA mixed data visualization - Global")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+    if dim==3:
+        fig = plt.figure(figsize=(9, 7))
+        ax = fig.add_subplot(111, projection="3d")
+
+        for i in clusters.keys():
+            mask = cluster_ids == i
+            ax.scatter(
+                X_all_pca[mask, 0],
+                X_all_pca[mask, 1],
+                X_all_pca[mask, 2],
+                alpha=0.7,
+                label=f"Cluster {i}"
+            )
+        ax.set_xlabel("PC1")
+        ax.set_ylabel("PC2")
+        ax.set_zlabel("PC3")
+        ax.set_title("PCA mixed data visualization 3D - Global")
+        ax.legend()
+
+        plt.tight_layout()
+        plt.show()
+
+
+
+    #Per cluster graphic
+
+    n_clusters = len(clusters)
+    n_cols = 2
+    n_rows = int(np.ceil(n_clusters / n_cols))
+
+    if dim==2:
         fig, axes = plt.subplots(n_rows, n_cols, figsize=(7 * n_cols, 5 * n_rows))
         axes = np.array(axes).reshape(-1)
 
-    for i, (X_cluster, y_cluster) in clusters.items():
-        X_cluster, y_cluster = clusters[i]
+        for idx, (i, (X_cluster, y_cluster)) in enumerate(clusters.items()):
 
-        df = X_cluster.copy() 
+            df = X_cluster.copy()
+            X_proc = preprocess.fit_transform(df)
 
-        X_proc = preprocess.fit_transform(df)
-        
-        pca = PCA(n_components=2)
-        X_pca = pca.fit_transform(X_proc)
+            pca = PCA(n_components=dim)
+            X_pca = pca.fit_transform(X_proc)
 
-
-        if visualization:
-            ax = axes[i - 1]
-
+            ax = axes[idx]
             ax.scatter(X_pca[:, 0], X_pca[:, 1], alpha=0.7)
             ax.set_xlabel("PC1")
             ax.set_ylabel("PC2")
@@ -218,9 +281,101 @@ def pca_mixed_data_visualization(clusters, categorical_cols, numerical_cols,visu
                 verticalalignment="top"
             )
 
-    if visualization:
-        for j in range(i, len(axes)):
-            fig.delaxes(axes[j])
+    if dim==3:
+        
+        fig = plt.figure(figsize=(7 * n_cols, 5 * n_rows))
+        axes = []
 
-        plt.tight_layout()
-        plt.show()
+        for i in range(n_rows * n_cols):
+            axes.append(fig.add_subplot(n_rows, n_cols, i + 1, projection="3d"))
+
+        for idx, (i, (X_cluster, y_cluster)) in enumerate(clusters.items()):
+
+            df = X_cluster.copy()
+            X_proc = preprocess.fit_transform(df)
+
+            pca = PCA(n_components=3)
+            X_pca = pca.fit_transform(X_proc)
+
+            ax = axes[idx]
+            ax.scatter(
+                X_pca[:, 0],
+                X_pca[:, 1],
+                X_pca[:, 2],
+                alpha=0.7
+            )
+
+            ax.set_xlabel("PC1")
+            ax.set_ylabel("PC2")
+            ax.set_zlabel("PC3")
+            ax.set_title(f"PCA mixed data visualization 3D - Cluster {i}")
+
+            ax.text2D(
+                0.02, 0.95,
+                f"n = {len(df)}",
+                transform=ax.transAxes
+            )
+
+
+    for j in range(idx + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+def update_summary(path, new_entry):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            summary = json.load(f)
+    else:
+        summary = []
+
+    summary.append(new_entry)
+
+    with open(path, 'w') as f:
+        json.dump(summary, f, indent=4)
+
+
+def global_json_calculation(path):
+
+    with open(path, "r") as f:
+        results = json.load(f)
+
+    total_samples = 0
+    weighted_accuracy = 0.0
+    weighted_bal_acc = 0.0
+    weighted_f1 = 0.0
+    weighted_inference_time = 0.0
+
+    for data in results:
+
+        n = data["cluster_len"]
+
+        weighted_accuracy += data["test_auroc"] * n
+        weighted_accuracy += data["test_accuracy"] * n
+        weighted_bal_acc += data["test_balance_accuracy"] * n
+        weighted_f1 += data["test_f1"] * n
+        weighted_inference_time += data["inference_time"]
+
+        total_samples += n
+
+    weighted_accuracy /= total_samples
+    weighted_bal_acc /= total_samples
+    weighted_f1 /= total_samples
+
+    global_metrics = {
+        "test_accuracy": weighted_accuracy,
+        "balanced_accuracy": weighted_bal_acc,
+        "f1": weighted_f1,
+        "dataset_size": total_samples,
+        "inference_time": weighted_inference_time,
+    }
+
+
+    results.append({"cluster_global": global_metrics})
+    
+    with open(path, "w") as f:
+        json.dump(results, f, indent=2)
